765e2927eb8aedf58b59529d710ac3598cdb1dc4
FerRguez <fer94_valdemoro@hotmail.es>
2015-05-01 20:20:40 +0200

    dialict

--- models/models.js
+++ models/models.js
@@ -5,7 +5,7 @@ var DB_name = (url[6]||null);
 var user = (url[2]||null);
 var pwd = (url[3]||null);
 var protocol = (url[1]||null);
-var dialect = (url[1]||null);
+var dialect = "sqlite";
 var port = (url[5]||null);
 var host = (url[4]||null);
 var storage = process.env.DATABASE_STORAGE;
@@ -14,7 +14,7 @@ var Sequelize = require('sequelize');
 
 var sequelize = new Sequelize(DB_name, user, pwd,
  {
- 	dialect: protocol,
+ 	dialect: dialect,
 	protocol: protocol,
 	port: port,
 	host: host,
